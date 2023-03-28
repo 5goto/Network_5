89357facd585990ae2f8cd4bdfaa77f2a47a760d
5goto <2breal1984@gmail.com>
2023-03-29 02:25:23 +1000

    1.0

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  let currentGalImage = $(".gal-item");
+  currentGalImage.click(function () {
+    const imageSrc = $(this).attr("src");
 
-	// ...
+    $(".gal-main").attr("src", imageSrc);
+  });
+});
 
-})
+const a = "fdfdfd.jpg";
+const b = "25";
+console.log("<img " + `src="${a}" width="${b}%"` + ">");
